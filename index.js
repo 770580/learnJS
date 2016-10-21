be3936c3d714eaf1a92aafb609cf03b22ed325dc
Anton <lascivolin@gmail.com>
2016-10-21 15:59:23 +0300

    function_str_buffer

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+function makeBuffer(){
+  let str = '' 
+  return function(value){
+    if(value == undefined) {
+      return str;
+    }
+    else {
+      str += value;
+    }
+  }
+}
