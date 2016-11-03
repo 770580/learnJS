137ce873f6bf615fb3b8bc238e343f261a32b6f7
Anton <lascivolin@gmail.com>
2016-11-03 12:41:52 +0300

    eslint airbnb

--- src/index.js
+++ src/index.js
@@ -1,86 +1,25 @@
-'use strict';
-function Machine(power) {
-  this._power = power;
-  this._enabled = false;
+const head = {
+  glasses: 1,
+};
 
-  this.enable = function() {
-    this._enabled = true;
-  };
+const table = {
+  pen: 3,
+  __proto__: head,
+};
 
-  this.disable = function() {
-    this._enabled = false;
-  };
-}
+const bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table,
+};
 
-function Fridge(power) {
-  Machine.call(this, power);
+const pockets = {
+  money: 2000,
+  __proto__: bed,
+};
 
-  let food = [];
-
-  this.addFood = (...items) => {
-    if (!this._enabled) {
-      throw new Error('Холодильник выключен');
-    }
-
-    let newFood = food.concat(items);
-
-    if (newFood.length > this._power / 100) {
-      throw new Error('Слишком много еды');
-    }
-
-    food = newFood;
-  }
-  this.getFood = () => food.slice();
-
-  let parentDisable = this.disable;
-  this.disable = () => {
-    if ( food.length > 0) {
-      throw new Error('в холодильнике есть еда');
-    }
-    parentDisable.call(this);
-  };
-
-  this.removeFood = (item) => {
-    let itemIndex = food.indexOf(item);
-    if ( itemIndex != -1) food.splice(itemIndex, 1);
-  }
-
-  this.filterFood = (func) => {
-    return food.filter(func);
-  }
-}
-
-var fridge = new Fridge(500);
-fridge.enable();
-fridge.addFood({
-  title: "котлета",
-  calories: 100
-});
-fridge.addFood({
-  title: "сок",
-  calories: 30
-});
-fridge.addFood({
-  title: "зелень",
-  calories: 10
-});
-fridge.addFood({
-  title: "варенье",
-  calories: 150
-});
-
-fridge.removeFood("нет такой еды"); // без эффекта
-alert( fridge.getFood().length ); // 4
-
-var dietItems = fridge.filterFood(function(item) {
-  return item.calories < 50;
-});
-
-dietItems.forEach(function(item) {
-  alert( item.title ); // сок, зелень
-  fridge.removeFood(item);
-});
-
-alert( fridge.getFood().length ); // 2
+console.log(pockets.pen);
+console.log(bed.glasses);
+console.log(table.money);
 
 module.hot.accept();
